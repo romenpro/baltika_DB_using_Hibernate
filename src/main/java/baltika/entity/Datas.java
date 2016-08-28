0999b9b3ba033a65264cbc7ecbea201ebc33c476
romenpro <romenpro@gmail.com>
2016-08-28 23:20:14 +0300

    Корректировка файла сущности DATAS

--- src/main/java/baltika/entity/Datas.java
+++ src/main/java/baltika/entity/Datas.java
@@ -29,5 +29,51 @@ public class Datas {
     public Datas() {
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getN_ob() {
+        return n_ob;
+    }
+
+    public void setN_ob(int n_ob) {
+        this.n_ob = n_ob;
+    }
+
+    public Date getSqlDate() {
+        return sqlDate;
+    }
+
+    public void setSqlDate(Date sqlDate) {
+        this.sqlDate = sqlDate;
+    }
+
+    public String getHh_mi() {
+        return hh_mi;
+    }
+
+    public void setHh_mi(String hh_mi) {
+        this.hh_mi = hh_mi;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    public double getPartOfSN() {
+        return partOfSN;
+    }
+
+    public void setPartOfSN(double partOfSN) {
+        this.partOfSN = partOfSN;
+    }
 }
